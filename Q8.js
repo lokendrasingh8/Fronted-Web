6c6add6f3168e49a754913d675e33bb42cafaede
LOKENDRA KUMAR <151841393+lokendrasingh8@users.noreply.github.com>
2025-11-12 08:44:39 +0530

    Delete Q8.js

--- Q8.js
+++
@@ -1,8 +0,0 @@
-let currentSalary = 40000;
-let incrementRate = 8;
-let salaryTable = [];
-for (let year = 1; year <= 5; year++) {
-  currentSalary += currentSalary * (incrementRate / 100);
-  salaryTable.push({ Year: year, Salary: Math.round(currentSalary) });
-}
-console.table(salaryTable);
